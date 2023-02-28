ff75d5e8c9eb444d61e0b02e882d2113ac143001
近藤　倫太 <tora.goraias@gmail.com>
2023-02-28 12:43:39 +0900

    ジャンプの仕様変更

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -15,6 +15,7 @@ public class PlayerMove : MonoBehaviour
 
     [SerializeField]
     [Header("床のタグ名")] string _groundTag = "Ground";
+    [SerializeField]float _distans = 1.0f;
     private Rigidbody rb;
     private bool _isGround = false;
     float moveX;
@@ -28,24 +29,19 @@ public class PlayerMove : MonoBehaviour
         moveX = Input.GetAxis("Horizontal") * _speed * _airSpeed;
         moveZ = Input.GetAxis("Vertical") * _speed * _airSpeed;
         rb.AddForce(new Vector3(moveX, 0, moveZ));
-        if(_isGround == true)
+
+        Vector3 rayPosition = transform.position + new Vector3();
+        Ray ray = new Ray(rayPosition, Vector3.down);
+        _isGround = Physics.Raycast(ray, _distans);
+        Debug.DrawRay(rayPosition, Vector3.down * _distans, Color.red);
+        if (Input.GetKeyDown("space"))
         {
-            if(Input.GetKeyDown("space"))
+            if (_isGround)
             {
                 _airSpeed = 0.2f;
                 Debug.Log("a");
                 rb.AddForce(0, _jumpPower, 0, ForceMode.Impulse);
-                _isGround = false;
             }
-        }
-    }
-    private void OnCollisionEnter(Collision collision)
-    {
-        //タグではなくプレイヤーの下にrayを飛ばして判定するようにする
-        if(collision.gameObject.tag == _groundTag)
-        {
-            _airSpeed = 1;
-            _isGround = true;
         }
     }
 }
