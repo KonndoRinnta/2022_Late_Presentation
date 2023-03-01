9a0b5acc8e66a3476fe6b589d10cebc031ac70f5
近藤　倫太 <tora.goraias@gmail.com>
2023-03-01 15:13:01 +0900

    Stage作成中

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -4,28 +4,14 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-    private GameObject player;
+    [SerializeField] GameObject player;
     private Vector3 offset;
     void Start()
     {
-        this.player = GameObject.Find("Player");
         offset = transform.position - player.transform.position;
     }
     void Update()
     {
         transform.position = player.transform.position + offset;
-        
-        //float mx = Input.GetAxis("Mouse X");
-        //float my = Input.GetAxis("Mouse Y");
-        
-        //if (Mathf.Abs(mx) > 0.001f)
-        //{
-        //    transform.RotateAround(player.transform.position, Vector3.up, mx);
-        //}
-
-        //if (Mathf.Abs(my) > 0.001f)
-        //{
-        //    transform.RotateAround(player.transform.position, transform.right, my);
-        //}
     }
 }
